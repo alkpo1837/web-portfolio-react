a023613acd104bb90419a525d5e4a7fbddad0482
alkpo1837 <capo.arthur@gmail.com>
2020-01-13 10:45:05 +0100

    Still trying some colors on About

--- src/components/About/SkillCard.js
+++ src/components/About/SkillCard.js
@@ -1,41 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SkillCard.scss";
 
 const SkillCard = ({ data }) => {
   const { category, skills } = data;
-  let [isOpened, setIsOpened] = useState(false);
-
-  function clickOnBtn(e) {
-    setIsOpened(!isOpened);
-    console.log(isOpened);
-  }
 
   return (
-    <div className="skill-card header-box-top-left-full-height border-bottom">
+    <div className="skill-card">
       <div className="header-title">
         <span>{category}</span>
-        <button
-          className="open-close-button"
-          onClick={clickOnBtn}
-          type="button"
-        >
-          {isOpened ? "Fermer" : "Voir"}
-        </button>
       </div>
       <div className="all-lines">
-        {isOpened &&
-          skills.map((skill, index) => {
-            const { iconFilename, name } = skill;
-            return (
-              <div key={index} className="skill">
-                <img
-                  src={require(`img/about/skills/${iconFilename}`)}
-                  alt={`Skill ${name}`}
-                />
-                <span>{name}</span>
-              </div>
-            );
-          })}
+        {skills.map((skill, index) => {
+          const { iconFilename, name } = skill;
+          return (
+            <div key={index} className="skill">
+              <img
+                src={require(`img/about/skills/${iconFilename}`)}
+                alt={`Skill ${name}`}
+              />
+              <span>{name}</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
